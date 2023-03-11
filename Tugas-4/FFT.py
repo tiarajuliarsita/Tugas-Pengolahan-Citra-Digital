3bf2896fa2a5cc4f1ad490198a1b8f5ee4399787
tiarajuliarsita <tiarajuliarsita@gmail.com>
2023-03-11 13:11:46 +0800

    FFT

---
+++ Tugas-4/FFT.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Load  the image
+img = cv2.imread('image02.png', 0)
+
+# Perform FFT on the image
+f = np.fft.fft2(img)
+fshift = np.fft.fftshift(f)
+magnitude_spectrum = 20*np.log(np.abs(fshift))
+
+# Perform inverse FFT to get the processed image
+f_ishift = np.fft.ifftshift(fshift)
+img_back = np.fft.ifft2(f_ishift)
+img_back = np.abs(img_back)
+
+# Display the results
+plt.subplot(121), plt.imshow(img, cmap='gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(magnitude_spectrum, cmap='gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
